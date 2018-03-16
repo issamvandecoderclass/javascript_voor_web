4347e090aa1902e7d53cd51cecdbbec0be89f143
issamvandecoderclass <issam.buamran@coderclass.nl>
2018-03-16 10:28:23 +0100

    Begin

---
+++ script.js
@@ -0,0 +1,106 @@
+function rechts() {
+  var elem = document.getElementById("animate");
+  var pos = parseInt(elem.style.left, 10);
+  console.log(pos);
+  console.log("Ik ga naar rechts!");
+  console.log("Ik word duizelig!");
+
+  if (isNaN(pos)) {
+    pos = 0;
+  }
+
+  var id = setInterval(frame, -15);
+  var tel = 0;
+  function frame() {
+    if (tel == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      tel++;
+      elem.style.left = pos + 'px';
+  }
+}
+}
+
+function Links() {
+  var elem = document.getElementById("animate");
+  var pos = parseInt(elem.style.left, 10);
+  console.log(pos);
+  console.log("Ik ga naar links!");
+  console.log("Aaaaaaaaaaaaaaaaaah")
+
+  if (isNaN(pos)) {
+    pos = 0;
+  }
+
+  var id = setInterval(frame, -15);
+  var tel = 0;
+  function frame() {
+    if (tel == 350) {
+      clearInterval(id);
+    } else {
+      pos--;
+      tel++;
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+
+function omlaag() {
+  var elem = document.getElementById("animate");
+  var pos = parseInt(elem.style.top, 10);
+  console.log(pos);
+  console.log("Ik ga naar omlaag!");
+  console.log("Oh wacht, ik vaaaaaaaaaaaaaaaaaaaaaaaaaal!");
+
+  if (isNaN(pos)) {
+    pos = 0;
+  }
+
+  var id = setInterval(frame, -15);
+  var tel = 0;
+  function frame() {
+    if (tel == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      tel++;
+      elem.style.top = pos + 'px';
+  }
+}
+}
+
+
+function omhoog() {
+  var elem = document.getElementById("animate");
+  var pos = parseInt(elem.style.top, 10);
+  console.log(pos);
+  console.log("Ik ga naar omhoog!");
+  console.log("De ruimte is echt mooi!");
+
+  if (isNaN(pos)) {
+    pos = 0;
+  }
+
+  var id = setInterval(frame, -15);
+  var tel = 0;
+  function frame() {
+    if (tel == 350) {
+      clearInterval(id);
+    } else {
+      pos--;
+      tel++;
+      elem.style.top = pos + 'px';
+    }
+  }
+}
+
+function bigImg(x) {
+    x.style.height = "250px";
+    x.style.width = "250px";
+}
+
+function normalImg(x) {
+    x.style.height = "100px";
+    x.style.width = "100px";
+}
